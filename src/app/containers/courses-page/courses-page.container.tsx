e0130500639ca3f6bf558bfc4bf4c76502742bc7
Juni Brosas <powerlogic1992@gmail.com>
2018-12-20 20:19:58 +0800

    add assets

--- src/app/containers/courses-page/courses-page.container.tsx
+++ src/app/containers/courses-page/courses-page.container.tsx
@@ -27,9 +27,42 @@ class CoursesPage extends React.Component<IProps> {
   render() {
     const { courses } = this.props;
     console.warn(courses);
-    return <div>
-      <button onClick={this.onAddCourse}>Add course</button>    
-    </div>
+    return (
+      <div className="animated fadeIn">
+        <header className="mb-5">
+          <nav className="navbar navbar-expand-lg">
+              <div className="container">
+                  <a className="navbar-brand" href="#">
+                      <img src="img/logo-inline.jpg" className="img-fluid" alt="Application Logo" width="250" />
+                  </a>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+                      aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse">
+                      <ul className="navbar-nav ml-auto nav-flex-icons">
+                          <li className="nav-item profile-group">
+                              <span className="profile name">John Doe</span>
+                              <span className="profile type">Learner</span>
+                          </li>
+                          <li className="nav-item avatar dropdown">
+                              <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true"
+                                  aria-expanded="true">
+                                  <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" className="rounded-circle z-depth-0" alt="avatar image" />
+                              </a>
+                              <div className="dropdown-menu dropdown-menu-right dropdown-secondary" aria-labelledby="navbarDropdownMenuLink-5">
+                                  <a className="dropdown-item waves-effect waves-light" href="#">Action</a>
+                                  <a className="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                                  <a className="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+                              </div>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </nav>
+        </header>
+      </div>
+    )
   }
 
   onAddCourse = () => {
